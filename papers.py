7ba26a4c2740dc5ec54ea3fad259a492234f09eb
Sam Novak <sam@server.local>
2014-11-07 15:55:25 -0500

    Updated doc strings, and helper functions
    Updated doc strings to reflect what each helper function does. Changed
    return statements to output True or False back to main function. Main
    program outputs list of strings

--- papers.py
+++ papers.py
@@ -2,10 +2,9 @@
 
 """ Computer-based immigration office for Kanadia """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Sam Novak and Suraj Narayanan'
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Sam & Suraj"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
@@ -17,7 +16,6 @@ import json
 
 
 def decide(input_file, watchlist_file, countries_file):
-
     """
     Decides whether a traveller's entry into Kanadia should be accepted
 
@@ -49,14 +47,13 @@ def decide(input_file, watchlist_file, countries_file):
             'entry_check': check_entry_reason(item, countries)
         }
         print(conditions)
-        if conditions['med_check'] == 'Quarantine':
+        if conditions['med_check']:
             output_list.append('Quarantine')
-
-        elif conditions['comp_check'] == 'Reject':
+        elif conditions['comp_check']:
             output_list.append('Reject')
-        elif conditions['entry_check'] == 'Reject':
+        elif conditions['entry_check']:
             output_list.append('Reject')
-        elif conditions['watch_check'] == 'Secondary':
+        elif conditions['watch_check']:
             output_list.append('Secondary')
         else:
             output_list.append('Accept')
@@ -64,31 +61,32 @@ def decide(input_file, watchlist_file, countries_file):
     return output_list
 
 
-
-
 def check_medical_advisory(item, countries):
     """
-    Checks whether a passport number is five sets of five alpha-number characters separated by dashes
-    :param passport_number: alpha-numeric string
-    :return: Boolean; True if the format is valid, False otherwise
+    Checks if the traveller's has passed through or has a home country with a medical advisory
+    :param item: dictionary containing traveller information
+    :param countries: dictionary containing list of countries with medical advisory information
+    :return: Boolean; True if format meets any of the conditions, False otherwise
     """
-
-    """Condition to check if the traveller is coming from country with medical advisory"""
     from_country = item["from"]["country"].upper()
 
     if countries[from_country]["medical_advisory"] != "":
-        return 'Quarantine'
+        return True
+
     if "via" in item:
         via_country = item["via"]["country"].upper()
         if countries[via_country]["medical_advisory"] != "":
-            return 'Quarantine'
+            return True
+
+    return False
 
 
 def check_watchlist(item, watchlist):
     """
-    Checks whether a passport number is five sets of five alpha-number characters separated by dashes
-    :param passport_number: alpha-numeric string
-    :return: Boolean; True if the format is valid, False otherwise
+    Checks whether a traveller's passport, last name, or first and last name is contained in a traveller watchlist
+    :param item: dictionary containing traveller information
+    :param watchlist: dictionary containing list of travellers that require additional screening
+    :return: Boolean; True if traveller meets any of the conditions, False otherwise
     """
     for watch_item in watchlist:
         """Condition to check if the traveller is on the watchlist"""
@@ -100,14 +98,16 @@ def check_watchlist(item, watchlist):
                 last_name == item["last_name"].upper() or\
                 passport_number == item["passport"].upper():
 
-            return 'Secondary'
+            return True
+
+    return False
 
 
 def check_completeness(item):
     """
-    Checks whether a passport number is five sets of five alpha-number characters separated by dashes
-    :param passport_number: alpha-numeric string
-    :return: Boolean; True if the format is valid, False otherwise
+    Checks whether a traveller has all the required information
+    :param item: dictionary containing traveller information
+    :return: Boolean; True if invalid birth date, passport, or missing req info. False otherwise
     """
     """Condition to check if the traveller details are incomplete"""
     if valid_passport_format(item["passport"]) and valid_date_format(item["birth_date"]):
@@ -122,37 +122,48 @@ def check_completeness(item):
                 not item["from"]["city"] or\
                 not item["from"]["region"] or\
                 not item["from"]["country"]:
-            return 'Reject'
+            return True
     else:
-        return 'Reject'
+        return True
+
+    return False
 
 
 def check_entry_reason(item, countries):
-    """If the reason for entry is to visit and the visitor has a passport from a country from which a visitor
-    visa is required, the traveller must have a valid visa. A valid visa is one that is less than two years
-    old."""
+    """
+    Checks whether a traveller's has a valid visa based on the reason for entry
+    :param item: dictionary containing traveller information
+    :param countries: dictionary containing a list of countries and visa requirements
+    :return: Boolean; True if fails to meet requirements for given entry reason, False otherwise
+    """
 
     home_country = item["home"]["country"]
-    if item["entry_reason"] == "returning":
 
-        if home_country == "KAN":
-            return
+    if item["entry_reason"] == "visit":
+        if "visa" in item:
+            if home_country == countries[home_country]["code"] and valid_visa(item["visa"]["code"], item["visa"]["date"]):
+                return False
+            else:
+                return True
         else:
-            return "Reject"
-    elif item["entry_reason"] == "visit":
+            return True
 
-        if home_country == countries[home_country]["code"] and valid_visa(item["visa"]["code"], item["visa"]["date"]):
-            return
-        else:
-            return "Reject"
     elif item["entry_reason"] == "transit":
-
-        if home_country == countries[home_country]["code"] and valid_visa(item["visa"]["code"], item["visa"]["date"]):
-            return
+        if "visa" in item:
+            if home_country == countries[home_country]["code"] and valid_visa(item["visa"]["code"], item["visa"]["date"]):
+                return False
+            else:
+                return True
         else:
-            return "Reject"
+            return True
+
+    elif item["entry_reason"] == "returning":
+        if home_country == "KAN":
+            return False
+        else:
+            return True
     else:
-        return "Reject"
+        return True
 
 
 def valid_passport_format(passport_number):
@@ -171,14 +182,16 @@ def valid_passport_format(passport_number):
 
 def valid_visa(visa_number, date):
     """
-    Checks whether a visa number is two sets of five alpha-number characters separated by dashes
+    Checks whether a visa number is two sets of five alpha-number characters separated by dashes,
+    and has been issued within 2 years (730 days).
     :param visa_number: alpha-numeric string
-    :return: Boolean; True if the format is valid, False otherwise
+    :param date: date when visa was issued
+    :return: Boolean; True if the visa is valid, False otherwise
     """
     visa_format = re.compile('\w{5}-\w{5}$')
 
-
     if visa_format.match(visa_number) and valid_date_format(date):
+
         current_date = str(datetime.date.today())
         current_date = datetime.datetime.strptime(current_date, "%Y-%m-%d")
         visa_date = datetime.datetime.strptime(date, "%Y-%m-%d")
@@ -203,5 +216,5 @@ def valid_date_format(date_string):
     except ValueError:
         return False
 
-print(decide('example2.json', 'watchlist.json', 'countries.json'))
+print(decide('example_entries.json', 'watchlist.json', 'countries.json'))
 #print(valid_visa('YD77Y-1MH6U', '2009-11-01'))
